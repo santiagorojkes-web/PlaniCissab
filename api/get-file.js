e510d6d0a85abf0f4733b830c702b6783f22eb88
santiagorojkes-web <santiagorojkes@gmail.com>
2026-08-19 13:33:08 -0300

    Refactor file download to use getDriveAccessToken

--- api/get-file.js
+++ api/get-file.js
@@ -1,4 +1,5 @@
-// Descarga un archivo de Drive usando las credenciales de moadon
+const { getDriveAccessToken } = require('./_drive_auth');
+
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
@@ -9,24 +10,12 @@ module.exports = async function handler(req, res) {
   if (!fileId) return res.status(400).json({ error: 'Falta fileId' });
 
   try {
-    const tokenRes = await fetch('https://oauth2.googleapis.com/token', {
-      method: 'POST', headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        client_id: process.env.GOOGLE_CLIENT_ID,
-        client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-        grant_type: 'refresh_token'
-      })
-    });
-    const { access_token } = await tokenRes.json();
-    if (!access_token) return res.status(500).json({ error: 'No se pudo autenticar' });
-
+    const accessToken = await getDriveAccessToken();
     const fileRes = await fetch(`https://www.googleapis.com/drive/v3/files/${fileId}?alt=media`, {
-      headers: { Authorization: `Bearer ${access_token}` }
+      headers: { Authorization: `Bearer ${accessToken}` }
     });
     const buffer = await fileRes.arrayBuffer();
     const base64 = Buffer.from(buffer).toString('base64');
-
     return res.status(200).json({ success: true, base64 });
   } catch (err) {
     return res.status(500).json({ error: err.message });
